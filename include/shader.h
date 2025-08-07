ea0f52c7e6b883f82d9517b78c00fee838baaf13
zalos <satya123m4@gmail.com>
2025-08-07 14:09:24 +0530

    add: shader.h

---
+++ include/shader.h
@@ -0,0 +1,115 @@
+#ifndef SHADER_H
+#define SHADER_H
+#include "../vendor/glad/include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class Shader {
+    // > read files
+    // > create shaders
+    // > compile them
+    // > show errors if any
+    
+unsigned int ID;
+public:
+    // constructor
+    Shader(const char* vertexPath, const char* fragmentPath){
+        // Read Source      
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+
+        // check if trouble initializing ifstream
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+        try{
+            // open files
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+
+            // read file buffer into string stream
+            std::stringstream fShaderStream, vShaderStream;
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+
+            // close files
+            vShaderFile.close();
+            fShaderFile.close();
+
+            // convert stream into string
+            vertexCode =  vShaderStream.str();
+            fragmentCode =  fShaderStream.str();
+        }
+        catch(std::ifstream::failure& e){
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        }
+
+        // sanitize source for compilation
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        // Create & Compile Source
+        unsigned int vertex, fragment;
+
+        // Compile Vertex Shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+
+        // check for vertex compilation errors
+        checkCompilationErrors(vertex, "VERTEX");
+
+        // Compile Fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+
+        // check for fragment compilation errors
+        checkCompilationErrors(fragment, "FRAGMENT");
+
+        // SHADER PROGRAM
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        // check for program compilation errors
+        checkCompilationErrors(ID, "PROGRAM");
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+    // Use/Activate Shader
+    void use(){
+        glUseProgram(ID);
+    }
+
+private: 
+    void checkCompilationErrors(unsigned int shader, std::string type){
+        int success;
+        char infoLog[1024];
+        if(type=="PROGRAM"){
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success){
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+        else {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
+#include "../include/shader.h"
+
+#include <cmath>
 #include <iostream>
+#include <iterator>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
@@ -10,20 +14,6 @@ void process_input(GLFWwindow* window);
 // GLOBAL VARIABLES 
 const unsigned int SCR_WIDTH = 1080;
 const unsigned int SCR_HEIGHT= 720;
-
-const char* vertexShaderSource = 
-    "#version 330 core\n"
-    "layout (location=0) in vec3 aPos;\n"
-    "void main(){\n"
-    "gl_Position = vec4(aPos, 1.0f);\n"
-    "}";
-
-const char* fragmentShaderSource = 
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main(){\n"
-    "FragColor = vec4(1.0, 0.5f, 0.2f, 1.0f);\n"
-    "}";
 
 // Buffer Usage:
 // > Generate a buffer and store it's id
@@ -54,50 +44,7 @@ int main(){
         return -1;
     }
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-    // Compile Vertex Shader
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if(!success){
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // Compile Fragment Shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
-    if(!success){
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Linking Errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-
-    if(!success){
-        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader ourShader("../src/shaders/shader.vert", "../src/shaders/shader.frag");
 
     // VERTEX DATA
     // Triangle Vertices
@@ -108,11 +55,19 @@ int main(){
     // };
     
     // Rectangle Vertices
+    // float vertices[] = {
+    //     -0.5f, 0.5f, 0.0f,
+    //     -0.5f, -0.5f, 0.0f,
+    //     0.5f, -0.5f, 0.0f,
+    //     0.5f, 0.5f, 0.0f
+    // };
+    
     float vertices[] = {
-        -0.5f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f
+        // vertex coords        // colors
+        -0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.0f,      0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f,       0.2f, 0.4f, 0.7f,
     };
 
     unsigned int indices[] = {
@@ -136,8 +91,15 @@ int main(){
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // vertex attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // RENDER LOOP
     while(!glfwWindowShouldClose(window)){
@@ -148,8 +110,9 @@ int main(){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw triangle
-        glUseProgram(shaderProgram);
+        // draw 
+        ourShader.use();
+
         glBindVertexArray(VAO);
         // glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
